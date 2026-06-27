059249a54cde3c49c072e5a860bcb773a143440e
Hulki24 <121166668+Hulki24@users.noreply.github.com>
2026-06-27 15:59:54 +0200

    Implement Fritz5G configuration flow

--- custom_components/fritz_5g/config_flow.py
+++ custom_components/fritz_5g/config_flow.py
@@ -1,1 +1,39 @@
+from __future__ import annotations
 
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+
+from .const import DOMAIN
+
+
+class Fritz5GConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """FRITZ! 5G Config Flow."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+
+        errors = {}
+
+        if user_input is not None:
+
+            return self.async_create_entry(
+                title=user_input[CONF_HOST],
+                data=user_input,
+            )
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_HOST, default="fritz.box"): str,
+                vol.Required(CONF_USERNAME): str,
+                vol.Required(CONF_PASSWORD): str,
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
+        )
