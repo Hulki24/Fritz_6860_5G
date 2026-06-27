afa6cd9c75bdc73433d69d8df016fd521c831d10
Hulki24 <121166668+Hulki24@users.noreply.github.com>
2026-06-27 16:31:54 +0200

    Integrate Fritz5GCoordinator into setup entry
    Added Fritz5GCoordinator for handling data and setup.

--- custom_components/fritz_5g/__init__.py
+++ custom_components/fritz_5g/__init__.py
@@ -2,6 +2,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .coordinator import Fritz5GCoordinator
 
 PLATFORMS = ["sensor"]
 
@@ -15,10 +16,21 @@ async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
 ):
+    coordinator = Fritz5GCoordinator(
+        hass,
+        entry.data,
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
     await hass.config_entries.async_forward_entry_setups(
         entry,
         PLATFORMS,
     )
+
     return True
 
 
@@ -26,7 +38,12 @@ async def async_unload_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
 ):
-    return await hass.config_entries.async_unload_platforms(
+    unload_ok = await hass.config_entries.async_unload_platforms(
         entry,
         PLATFORMS,
     )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
